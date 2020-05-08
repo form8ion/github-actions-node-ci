3e3b62bb93cae4ed034f57c15b2a915923bf9d52
Matt Travi <programmer@travi.org>
2020-05-07 23:14:34 -0500

    feat(config): populated the workflow further

--- src/config-scaffolder-test.js
+++ src/config-scaffolder-test.js
@@ -30,7 +30,29 @@ suite('config scaffolder', () => {
       yamlWriter.default,
       `${pathToCreatedWorkflowsDirectory}/node-ci.yml`,
       {
-        name: 'Node.js CI'
+        name: 'Node.js CI',
+        on: {
+          push: {branches: ['master']},
+          pull_request: {branches: ['master']}
+        },
+        jobs: {
+          build: {
+            'runs-on': 'ubuntu-latest',
+            steps: [
+              {uses: 'actions/checkout@v2'},
+              {
+                name: 'Setup node',
+                uses: 'actions/setup-node@v1',
+                with: {'node-version': '12.x'}
+              },
+              {run: 'npm clean-install'},
+              {
+                run: 'npm test',
+                env: {CI: true}
+              }
+            ]
+          }
+        }
       }
     );
   });
--- src/config-scaffolder.js
+++ src/config-scaffolder.js
@@ -5,7 +5,29 @@ export default async function ({projectRoot}) {
   return writeYaml(
     `${await mkdir(`${projectRoot}/.github/workflows`)}/node-ci.yml`,
     {
-      name: 'Node.js CI'
+      name: 'Node.js CI',
+      on: {
+        push: {branches: ['master']},
+        pull_request: {branches: ['master']}
+      },
+      jobs: {
+        build: {
+          'runs-on': 'ubuntu-latest',
+          steps: [
+            {uses: 'actions/checkout@v2'},
+            {
+              name: 'Setup node',
+              uses: 'actions/setup-node@v1',
+              with: {'node-version': '12.x'}
+            },
+            {run: 'npm clean-install'},
+            {
+              run: 'npm test',
+              env: {CI: true}
+            }
+          ]
+        }
+      }
     }
   );
 }
