eea354847c965ca742c0c3fa0a81c2d2ef34c366
Matt Travi <programmer@travi.org>
2021-01-03 15:54:30 -0600

    refactor(js-yaml): used the modern api

--- test/integration/features/step_definitions/ci-steps.js
+++ test/integration/features/step_definitions/ci-steps.js
@@ -1,7 +1,7 @@
 import {promises as fs} from 'fs';
 import makeDir from 'make-dir';
 import {Given, Then} from '@cucumber/cucumber';
-import {safeDump, safeLoad} from 'js-yaml';
+import {dump, load} from 'js-yaml';
 import {assert} from 'chai';
 
 Given('a CI workflow exists', async function () {
@@ -9,13 +9,13 @@ Given('a CI workflow exists', async function () {
 
   await fs.writeFile(
     `${workflowsDirectory}/node-ci.yml`,
-    safeDump({on: {push: {branches: this.existingBranches}}})
+    dump({on: {push: {branches: this.existingBranches}}})
   );
 });
 
 Then('the ci config remains unchanged', async function () {
   assert.deepEqual(
-    safeLoad(await fs.readFile(`${process.cwd()}/.github/workflows/node-ci.yml`)).on.push.branches,
+    load(await fs.readFile(`${process.cwd()}/.github/workflows/node-ci.yml`)).on.push.branches,
     this.existingBranches
   );
 });
