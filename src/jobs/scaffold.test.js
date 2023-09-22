5bb97bd9ca163ba89f27dfdeff94a3b71a79465e
Matt Travi <programmer@travi.org>
2023-09-21 23:42:12 -0500

    test(unit): converted the job scaffolder tests to vitest
    for #946

---
+++ src/jobs/scaffold.test.js
@@ -0,0 +1,51 @@
+import {
+  scaffoldCheckoutStep,
+  scaffoldNodeSetupStep,
+  scaffoldDependencyInstallationStep,
+  scaffoldVerificationStep
+} from '@form8ion/github-workflows-core';
+
+import {afterEach, beforeEach, describe, vi, it, expect} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import {matrixVerification, nvmrcVerification} from './scaffolder';
+
+vi.mock('@form8ion/github-workflows-core');
+
+describe('jobs scaffolder', () => {
+  const checkoutStep = any.simpleObject();
+  const setupNodeStep = any.simpleObject();
+  const installDependenciesStep = any.listOf(any.simpleObject);
+  const executeVerificationStep = any.simpleObject();
+
+  beforeEach(() => {
+    scaffoldCheckoutStep.mockReturnValue(checkoutStep);
+    scaffoldDependencyInstallationStep.mockReturnValue(installDependenciesStep);
+    scaffoldVerificationStep.mockReturnValue(executeVerificationStep);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should create an nvmrc verification job', () => {
+    when(scaffoldNodeSetupStep).calledWith({versionDeterminedBy: 'nvmrc'}).mockReturnValue(setupNodeStep);
+
+    expect(nvmrcVerification()).toEqual({
+      'runs-on': 'ubuntu-latest',
+      steps: [checkoutStep, setupNodeStep, ...installDependenciesStep, executeVerificationStep]
+    });
+  });
+
+  it('should create a matrix verification job', () => {
+    const nodeEnginesMatrix = any.listOf(any.integer);
+    when(scaffoldNodeSetupStep).calledWith({versionDeterminedBy: 'matrix'}).mockReturnValue(setupNodeStep);
+
+    expect(matrixVerification(nodeEnginesMatrix)).toEqual({
+      'runs-on': 'ubuntu-latest',
+      strategy: {matrix: {node: nodeEnginesMatrix}},
+      steps: [checkoutStep, setupNodeStep, ...installDependenciesStep, executeVerificationStep]
+    });
+  });
+});
