d3a9caac079d4d2fdf4897fadb570e55219d5ae4
Matt Travi <programmer@travi.org>
2023-09-24 08:09:13 -0500

    test(unit): converted the meerge-branches test to vitest
    for #946

---
+++ src/lifter/branches/merge-branches.test.js
@@ -0,0 +1,15 @@
+import {describe, it, expect} from 'vitest';
+import any from '@travi/any';
+
+import merge from './merge-branches';
+
+describe('merge branch lists', () => {
+  it('should append additional branches to the list', () => {
+    const existingBranches = any.listOf(any.word);
+    const additionalBranches = any.listOf(any.word);
+    const commonBranches = any.listOf(any.word);
+
+    expect(merge([...existingBranches, ...commonBranches], [...additionalBranches, ...commonBranches]))
+      .toEqual([...existingBranches, ...commonBranches, ...additionalBranches]);
+  });
+});
