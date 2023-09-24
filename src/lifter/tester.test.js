a40136430fdcb17c763fb33710a4a9dd4e61d085
Matt Travi <programmer@travi.org>
2023-09-23 22:05:50 -0500

    test(unit): converted the predicate tests to vitest
    for #946

---
+++ src/lifter/tester.test.js
@@ -0,0 +1,29 @@
+import {fileExists} from '@form8ion/core';
+
+import {afterEach, describe, it, vi, expect} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import testThatCiWorkflowExists from './tester';
+
+vi.mock('@form8ion/core');
+
+describe('predicate', () => {
+  const projectRoot = any.string();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return `false` when no ci workflow file exists', async () => {
+    fileExists.mockResolvedValue(false);
+
+    expect(await testThatCiWorkflowExists({projectRoot})).toBe(false);
+  });
+
+  it('should return `true` when a ci workflow file exists', async () => {
+    when(fileExists).calledWith(`${projectRoot}/.github/workflows/node-ci.yml`).mockResolvedValue(true);
+
+    expect(await testThatCiWorkflowExists({projectRoot})).toBe(true);
+  });
+});
