a438e6e6c3920e5d5f47120d3fb376b5277ea302
Matt Travi <programmer@travi.org>
2023-11-08 13:37:16 -0600

    test(unit): converted the final mocha test to vitest
    closes #946

---
+++ src/lifter/jobs/steps-lifter.test.js
@@ -0,0 +1,32 @@
+import {afterEach, describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import liftStep from './step-lifter.js';
+import liftSteps from './steps-lifter.js';
+
+vi.mock('./step-lifter.js');
+
+describe('steps lifter', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should remove the step for reading the `.nvmrc` and apply the step lifter to each step', () => {
+    const nvmrcReadStep = {...any.simpleObject(), id: 'nvm'};
+    const firstStep = any.simpleObject();
+    const lastStep = any.simpleObject();
+    const liftedFirstStep = any.simpleObject();
+    const liftedLastStep = any.listOf(any.simpleObject);
+    const liftedNvmrcReadStep = any.listOf(any.simpleObject);
+    const steps = [firstStep, nvmrcReadStep, lastStep];
+    when(liftStep).calledWith(firstStep).mockReturnValue(liftedFirstStep);
+    when(liftStep).calledWith(nvmrcReadStep).mockReturnValue(liftedNvmrcReadStep);
+    when(liftStep).calledWith(lastStep).mockReturnValue(liftedLastStep);
+
+    const liftedSteps = liftSteps(steps);
+
+    expect(liftedSteps).not.toContain(liftedNvmrcReadStep);
+    expect(liftedSteps).toEqual([liftedFirstStep, ...liftedLastStep]);
+  });
+});
